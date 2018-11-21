549ca3e394576934112812ee35c50a973327a614
oqasem1 <oqasaem.euro@gmail.com>
2018-11-21 21:56:25 +0200

    Force build in AWS

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello Omar
+            Hello Omar!
           </p>
           <a
             className="App-link"
